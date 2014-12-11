1ddd7e332e622cb0598c65917546d2e8dcbd5e4a
Vito Caputo <vito.caputo@coreos.com>
2014-12-11 13:11:55 -0800

    rkt status: implement status
    Simple querying of container status by uuid is provided.
    
    The pid of the container's stage1, an exited boolean, and per-imageid
    exit status codes are printed when available.
    
    Container exit synchronization occurs when --wait is specified.
    
    Examples:
     Already exited but not yet garbage collected container:
    
       $ rkt status --uuid 32e5c3d4-c851-4c46-bddc-b52c46449523
       pid=11219
       exited=true
       sha256-f9124faae79eb859b27581d9673a2d29a8e394a2685088753264dea276dd7571=0
       $
    
     Already exited and "marked" for garbage collection by rkt gc (same as above):
    
       $ rkt status --uuid 32e5c3d4-c851-4c46-bddc-b52c46449523
       pid=11219
       exited=true
       sha256-f9124faae79eb859b27581d9673a2d29a8e394a2685088753264dea276dd7571=0
       $
    
     Already exited and actively being discarded by rkt gc (treated as gone):
    
       $ bin/rkt status --uuid 874d5c50-ee5e-4cb9-9b30-c95778398e43
       Unable to query status: "874d5c50-ee5e-4cb9-9b30-c95778398e43" is being removed
       $
    
     Currently executing container:
    
       $ rkt status --uuid 8b06b7af-7d53-4723-bc0c-fe6e47463fcb
       pid=12227
       exited=false
       $
    
     Waiting on potentially executing container to exit for exit status:
    
       $ time bin/rkt status --uuid ec5df2c6-b5d9-495d-aea0-c2cf5da706c2 --wait
       pid=12319
       exited=true
       sha256-f9124faae79eb859b27581d9673a2d29a8e394a2685088753264dea276dd7571=0
    
       real    0m21.470s  <<< note delay before returning
       user    0m0.028s
       sys     0m0.004s
       $
    
       $ time bin/rkt status --uuid ec5df2c6-b5d9-495d-aea0-c2cf5da706c2 --wait
       pid=12319
       exited=true
       sha256-f9124faae79eb859b27581d9673a2d29a8e394a2685088753264dea276dd7571=0
    
       real    0m0.033s  <<< note zero delay when already exited
       user    0m0.024s
       sys     0m0.008s
       $

--- rkt/status.go
+++ rkt/status.go
@@ -2,22 +2,143 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/appc/spec/schema/types"
+	"github.com/coreos/rocket/pkg/lock"
+)
 
 var (
 	cmdStatus = &Command{
 		Name:    "status",
 		Summary: "Check the status of a rkt job",
-		Usage:   "UUID",
+		Usage:   "--uuid UUID [--wait]",
 		Run:     runStatus,
 	}
+	flagContainerUUID types.UUID
+	flagWait          bool
 )
 
 func init() {
 	commands = append(commands, cmdStatus)
+	cmdStatus.Flags.Var(&flagContainerUUID, "uuid", "uuid of container to query the status of")
+	cmdStatus.Flags.BoolVar(&flagWait, "wait", false, "toggle waiting for the container to exit if running")
 }
 
 func runStatus(args []string) (exit int) {
-	fmt.Println("Not implemented.")
+	isGarbage := false
+
+	if flagContainerUUID.Empty() {
+		fmt.Fprintf(os.Stderr, "--uuid required\n")
+		return 1
+	}
+
+	cp := filepath.Join(containersDir(), flagContainerUUID.String())
+	l, err := lock.NewLock(cp)
+	if err != nil {
+		cp = filepath.Join(garbageDir(), flagContainerUUID.String())
+		l, err = lock.NewLock(cp)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to open lock for container %q: %v\n", flagContainerUUID.String(), err)
+			return 2
+		}
+		isGarbage = true
+	}
+	defer l.Close()
+
+	exited := true
+	if flagWait && !isGarbage {
+		err = l.SharedLock()
+	} else {
+		err = l.TrySharedLock()
+		if err == lock.ErrLocked {
+			if isGarbage {
+				// container is exited and being deleted, we can't reliably query its status, it's effectively gone.
+				fmt.Fprintf(os.Stderr, "Unable to query status: %q is being removed\n", flagContainerUUID.String())
+				return 3
+			}
+			exited = false
+			err = nil
+		}
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Lock error: %v\n", err)
+		return 4
+	}
+
+	if err := printStatus(cp, exited); err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to print status: %v\n", err)
+		return 5
+	}
+
+	return 0
+}
+
+// printStatus prints the container's pid and per-app status codes
+func printStatus(cpath string, exited bool) error {
+	pid, err := getIntFromFile(filepath.Join(cpath, "pid"))
+	if err != nil {
+		return err
+	}
+
+	stats, err := getStatuses(cpath)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("pid=%d\nexited=%v\n", pid, exited)
+	for app, stat := range stats {
+		fmt.Printf("%s=%d\n", app, stat)
+	}
+	return nil
+}
+
+// getStatuses returns a map of imageId:status codes for the given container
+func getStatuses(cpath string) (map[string]int, error) {
+	sdir := filepath.Join(cpath, "stage1/rkt/status")
+	ls, err := ioutil.ReadDir(sdir)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read status directory: %v", err)
+	}
+
+	stats := make(map[string]int)
+	for _, ent := range ls {
+		if ent.IsDir() {
+			continue
+		}
+		s, err := getIntFromFile(filepath.Join(sdir, ent.Name()))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to get status of app %q: %v\n", ent.Name(), err)
+			continue
+		}
+		stats[ent.Name()] = s
+	}
+
+	return stats, err
+}
+
+// getIntFromFile reads an integer string from the named file
+func getIntFromFile(path string) (i int, err error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	buf, err := ioutil.ReadAll(f)
+	if err != nil {
+		return
+	}
+
+	_, err = fmt.Sscanf(string(buf), "%d", &i)
+	if err != nil {
+		return
+	}
+
 	return
 }
